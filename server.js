afb6097506084a3081dcfa4b17bd057ae62afd10
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-06-26 00:47:27 -0500

    updated server.js. created routes/api-routes.js and html-routes.js

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));	
 app.use(express.json());	
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/html-routes")(app)
+require("./routes/api-routes")(app)
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+
+module.exports = app;
