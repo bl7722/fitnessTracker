a877c0637d20eb89180fae10449198d23b47da14
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-13 04:45:46 -0500

    updated html-routes.js,server.js.trying to fix node_modules issue

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname,"../public/index.html"));
+});
+
+router.get("/exercise", function(request, response) {
+    response.sendFile(path.join(__dirname,"../public/exercise.html"));
+});
+
+router.get("/stats", function(request, response) {
+    response.sendFile(path.join(__dirname,"../public/stats.html"));
+});
+
+module.exports = router;
